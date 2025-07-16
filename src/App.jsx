945f0f286f661f6106fc527fb854deb0d2103f4d
Arijit Banerjee <arijitbanerjee873@gmail.com>
2025-07-16 19:22:50 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Button } from "./components/ui/button";
 const App = () => {
   return (
     <div className="min-h-screen flex justify-center items-center">
-      <Button>Welcome to Unpaid-U</Button>
+      <Button>Welcome to Unpaid-U.com</Button>
     </div>
   );
 };
