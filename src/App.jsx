025f0b239fdc632f3af0b13f201ebe1ec96bb82d
ArijitBanerjee <arijitbanerjee873@gmail.com>
2025-07-16 17:37:49 +0530

    added tailwind and shadcn library

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "./components/ui/button";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <Button>Welcome to Unpaid-U</Button>
+    </div>
+  );
 };
 
 export default App;
